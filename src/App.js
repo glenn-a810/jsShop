9db710db538a6d02b95d89d0b79ad19a3e4f01ec
Glenn <a810.glenn@gmail.com>
2022-08-08 21:49:04 +0900

    Router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { useState } from "react";
 import data from "./data";
 import Item from "./Item";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   const [food, setFood] = useState(data)
@@ -13,29 +14,47 @@ function App() {
         <Container>
           <Navbar.Brand href='#main'>CatShop</Navbar.Brand>
           <Nav className='me-auto'>
-            <Nav.Link href='#main'>Main</Nav.Link>
-            <Nav.Link href='#food'>Food</Nav.Link>
+            <Nav.Link href='/'>Main</Nav.Link>
+            <Nav.Link href='/food'>Food</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
+      {/*<Link to='/'>Main</Link>*/}
+      {/*<Link to='/food'>Food</Link>*/}
 
-      <div className='container'>
-        <div className='row'>
-          { food.map((parm,i)=>{
-            return (
-                <Item food={food[i]} />
-                // <div className='col-md-4'>
-                //   <img src={food[i].img} width='80%'/>
-                //   <h4>{food[i].title}</h4>
-                //   <p>{food[i].content}</p>
-                //   <p>{food[i].price}원</p>
-                // </div>
-            )
-          }) }
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className='main-bg'></div>
+            <div className='container'>
+              <div className='row'>
+                { food.map((parm,i)=>{
+                  return (
+                      <Item food={food[i]} />
+                  )
+                }) }
+              </div>
+            </div>
+          </>
+        } />
+        <Route path='/food' element={
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+              </div>
+              <div className="col-md-6">
+                <h4 className="pt-5">상품명</h4>
+                <p>상품설명</p>
+                <p>120000원</p>
+                <button className="btn btn-danger">주문하기</button>
+              </div>
+            </div>
+          </div>
+        } />
+      </Routes>
+
     </div>
   );
 }
