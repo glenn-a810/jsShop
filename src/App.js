d7dd67b70beecb4a695c1c06efd6f09c6d9bcf72
Glenn <a810.glenn@gmail.com>
2022-08-08 22:09:38 +0900

    Router

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { useState } from "react";
 import data from "./data";
 import Item from "./Item";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Outlet, Route, Routes, useNavigate } from "react-router-dom";
+import Detail from "./Detail";
 
 function App() {
   const [food, setFood] = useState(data)
+  const navigate = useNavigate()
 
   return (
     <div className="App">
@@ -14,8 +16,18 @@ function App() {
         <Container>
           <Navbar.Brand href='#main'>CatShop</Navbar.Brand>
           <Nav className='me-auto'>
-            <Nav.Link href='/'>Main</Nav.Link>
-            <Nav.Link href='/food'>Food</Nav.Link>
+            <Nav.Link onClick={()=>{
+              navigate('/')
+            }}>Main</Nav.Link>
+            <Nav.Link onClick={()=>{
+              navigate('/food')
+            }}>Food</Nav.Link>
+            <Nav.Link onClick={()=>{
+              navigate('/about')
+            }}>About</Nav.Link>
+            <Nav.Link onClick={()=>{
+              navigate('/event')
+            }} >Event</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -39,24 +51,39 @@ function App() {
           </>
         } />
         <Route path='/food' element={
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6">
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-              </div>
-              <div className="col-md-6">
-                <h4 className="pt-5">상품명</h4>
-                <p>상품설명</p>
-                <p>120000원</p>
-                <button className="btn btn-danger">주문하기</button>
-              </div>
-            </div>
-          </div>
+          <Detail />
         } />
+        <Route path='*' element={<div>404 not found</div>} />
+        <Route path='about' element={ <About /> } >
+          <Route path='member' element={ <div>Member</div> } />
+          <Route path='location' element={ <div>Location</div> } />
+        </Route>
+        <Route path='event' element={ <Event /> } >
+          <Route path='one' element={ <div>One</div> } />
+          <Route path='two' element={ <div>Two</div> } />
+        </Route>
       </Routes>
 
     </div>
   );
+}
+
+function About() {
+      return (
+          <div>
+            <h4>About Page</h4>
+            <Outlet />
+          </div>
+      )
+}
+
+function Event() {
+  return (
+      <div>
+        <h4>오늘의 이벤트</h4>
+        <Outlet />
+      </div>
+  )
 }
 
 export default App;
